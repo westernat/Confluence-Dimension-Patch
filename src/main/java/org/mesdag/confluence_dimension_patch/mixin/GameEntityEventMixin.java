5d8143814cdec28dc85f38e8957d56bf5158f34e
westernat <westernat233@163.com>
2025-07-17 21:03:17 +0800

    version

---
+++ src/main/java/org/mesdag/confluence_dimension_patch/mixin/GameEntityEventMixin.java
@@ -0,0 +1,19 @@
+package org.mesdag.confluence_dimension_patch.mixin;
+
+import net.neoforged.neoforge.event.entity.living.FinalizeSpawnEvent;
+import org.confluence.terraentity.event.GameEntityEvent;
+import org.mesdag.confluence_dimension_patch.common.OtherWorld;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(value = GameEntityEvent.class, remap = false)
+public abstract class GameEntityEventMixin {
+    @Inject(method = "mobFinalizeSpawn", at = @At("HEAD"), cancellable = true)
+    private static void cancel(FinalizeSpawnEvent event, CallbackInfo ci) {
+        if (event.getLevel().getLevel().dimension() != OtherWorld.LEVEL) {
+            ci.cancel();
+        }
+    }
+}
