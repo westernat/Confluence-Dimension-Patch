0b9059bddf2442972b09843d45f4b9a24d43c29b
westernat <westernat233@163.com>
2025-08-11 11:31:40 +0800

    update dependencies

--- src/main/java/org/mesdag/confluence_dimension_patch/mixin/BloodyTearItemMixin.java
+++ src/main/java/org/mesdag/confluence_dimension_patch/mixin/BloodyTearItemMixin.java
@@ -2,24 +2,16 @@ package org.mesdag.confluence_dimension_patch.mixin;
 
 import com.xiaohunao.terra_moment.common.item.BloodyTearItem;
 import net.minecraft.resources.ResourceKey;
-import net.minecraft.world.InteractionHand;
-import net.minecraft.world.InteractionResultHolder;
-import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import org.mesdag.confluence_dimension_patch.common.OtherWorld;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(BloodyTearItem.class)
-public class BloodyTearItemMixin {
-
+@Mixin(value = BloodyTearItem.class, remap = false)
+public abstract class BloodyTearItemMixin {
     @ModifyArg(method = "use", at = @At(value = "INVOKE", target = "Lcom/xiaohunao/heaven_destiny_moment/common/context/condition/common/LocationCondition$Builder;inDimension([Lnet/minecraft/resources/ResourceKey;)Lcom/xiaohunao/heaven_destiny_moment/common/context/condition/common/LocationCondition$Builder;"))
     private ResourceKey<Level>[] replace(ResourceKey<Level>[] dimension) {
         return new ResourceKey[]{OtherWorld.LEVEL};
     }
-
 }
